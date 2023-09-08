995def6454bbfb52ff699d9a808c641b0b2853bd
ProfL3304 <ProfL3304@lab.aca>
2023-09-08 16:40:06 -0500

    pong terminado

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.Events;
 
@@ -7,6 +8,9 @@ public class GameManager : MonoBehaviour
 {
     public int puntuacionJugadorIzq = 0;
     public int puntuacionJugadorDer = 0;
+
+    public TextMeshProUGUI textPuntajeIzq;
+    public TextMeshProUGUI textPuntajeDer;
 
     public event UnityAction OnPauseGame; // evento
     public event UnityAction OnRestartGame; // evento
@@ -18,10 +22,14 @@ public class GameManager : MonoBehaviour
         if (jugador == 0)
         {
             puntuacionJugadorIzq = puntuacionJugadorIzq + 1;
+            textPuntajeIzq.text = puntuacionJugadorIzq.ToString();
         }else 
         {
             puntuacionJugadorDer++;
+            textPuntajeDer.text = puntuacionJugadorDer.ToString();
         }
+        textPuntajeIzq.gameObject.SetActive(true);
+        textPuntajeDer.gameObject.SetActive(true);
         OnPauseGame.Invoke();
     }
 
@@ -30,6 +38,8 @@ public class GameManager : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space))
         {
             // Reiniciar el juego
+            textPuntajeIzq.gameObject.SetActive(false);
+            textPuntajeDer.gameObject.SetActive(false);
             OnRestartGame.Invoke();
         }
     }
